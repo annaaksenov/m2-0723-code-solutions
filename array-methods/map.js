8cb6586e461ec31a27b84441c1ea15ced43d3f18
Anna Aksenov <annaaksenov8@gmail.com>
2023-07-22 17:48:07 +0000

    Modified

--- array-methods/map.js
+++ array-methods/map.js
@@ -4,4 +4,6 @@ const arr1 = prices.map((num) => ({
   price: num,
   salePrice: num / 2,
 }));
-console.log(arr1);
+console.log('Price objs: ', arr1);
+const arr2 = prices.map((num) => `$${num}`);
+console.log('Formatted prices: ', arr2);
