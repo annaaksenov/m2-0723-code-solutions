1c3ea77be6db6c4e0decd80ec9b90812a9d92f03
Anna Aksenov <annaaksenov8@gmail.com>
2023-07-25 01:34:35 +0000

    Started to arrow event listener

---
+++ javascript-carousel/main.js
@@ -0,0 +1,16 @@
+/** ROUGH SKETCH:
+ * const currentIndex = 0;
+ * const eachImg = document.querySelectorAll('img);
+ * eventHandlerFunction that listens for a click from either the prog bar or arrows.
+ * In the Func
+ * const currentIndex = eachImg[currentIndex]... iterates thru imgs.
+ * Needs to incremment or decrement depending on which arrow or what dot is clicked.
+ */
+const leftArrow = document.querySelector('.fa-chevron-left');
+/* function handleLeft(event) {
+  if (!event.target.matches('.fa-chevron-left')) {
+    return;
+  }
+
+} */
+leftArrow.addEventListener('click');
