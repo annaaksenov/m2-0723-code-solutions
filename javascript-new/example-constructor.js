85f12279e0f7e4245a016b5b1213a8ff54c64fbd
Anna Aksenov <annaaksenov8@gmail.com>
2023-07-25 17:58:28 +0000

    Completed js-new

---
+++ javascript-new/example-constructor.js
@@ -0,0 +1,9 @@
+function ExampleConstructor() {}
+console.log('value of ExampleConstructor:', ExampleConstructor);
+console.log('typeof ExampleConstructor:', typeof ExampleConstructor);
+const example = new ExampleConstructor();
+console.log(example);
+console.log(
+  'example instanceof ExampleConstructor:',
+  example instanceof ExampleConstructor
+);
