a3ff9b78a594dbdcdfeffc42065f159dec5e44e6
Anna Aksenov <annaaksenov8@gmail.com>
2023-07-25 19:45:10 +0000

    Completed es6-operators

--- es6-operators/main.js
+++ es6-operators/main.js
@@ -4,11 +4,8 @@ const v2 = '';
 const v3 = undefined;
 const v4 = [3, 5, 7, 9];
 
-// const check1 = v1 && true ? console.log('v1 is truthy') : false;
-// const check2 = v2 && true ? console.log('v1 is truthy') : false;
 const check1 = console.log(v1 && true);
 const check2 = console.log(v2 && true);
-
 /* TODO:
  * Demonstrate how to execute an expression only if a variable is truthy.
  * In one expression, log "v1 is truthy" to the console only if `v1` is truthy.
