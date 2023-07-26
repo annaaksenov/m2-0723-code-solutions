21376e6d3689bed7d01213ee10948548a3c1838b
Anna Aksenov <annaaksenov8@gmail.com>
2023-07-26 03:25:57 +0000

    Completed js-ajax

---
+++ javascript-ajax/users.js
@@ -0,0 +1,18 @@
+function getUserName(name) {
+  const user = document.querySelector('#user-list');
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    console.log(xhr.status);
+    console.log(xhr.response);
+    const json = xhr.response;
+    for (let i = 0; i < json.length; i++) {
+      const li = document.createElement('li');
+      li.textContent = name;
+      li.append = user;
+    }
+  });
+  xhr.send();
+}
+getUserName();
