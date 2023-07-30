95d60df47922b3a5a3acba7603608169e122a709
Anna Aksenov <annaaksenov8@gmail.com>
2023-07-30 16:52:41 +0000

    Another code for carousel and might be more efficient

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,21 +1,36 @@
-let i = 0;
-const images = [
-  'images/001.png',
-  'images/025.png',
-  'images/039.png',
-  'images/004.png',
-  'images/007.png',
-];
+// const images = [
+//   'images/001.png',
+//   'images/025.png',
+//   'images/039.png',
+//   'images/004.png',
+//   'images/007.png',
+// ];
+// function slideShow() {
+//   document.querySelector('img').setAttribute('src', images[i]);
+//   if (i < images.length - 1) {
+//     i++;
+//   } else {
+//     i = 0;
+//   }
+//  setTimeout(slideShow, 3000);
+// }
+// window.onload = slideShow();
+//  const prevBtn = document.querySelector('.fa-chevron-left');
 
-const intervalID = setInterval(myCallback, 3000, images);
+//  prevBtn.addEventListener('click', function() {
+//    /* document.querySelector('img').setAttribute('src', images[i]); */;
+//   console.log('clicked');
+// });
+const images = document.querySelectorAll('img');
+let i = 1;
 
-function myCallback(images) {
-  document.querySelector('img').setAttribute('src', images[i]);
-  if (i < images.length - 1) {
-    i++;
-  } else {
+setInterval(() => {
+  images.forEach((img, i) => {
+    img.className = 'hidden';
+  });
+  if (i === images.length) {
     i = 0;
   }
-}
-
-console.log(intervalID);
+  images[i].className = '';
+  i++;
+}, 3000);
