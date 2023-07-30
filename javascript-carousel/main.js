c352ad7c1c86d3a565fbecf54109855622a85fad
Anna Aksenov <annaaksenov8@gmail.com>
2023-07-30 22:57:27 +0000

    Added a rough code for dot progress bar

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,9 +1,9 @@
 // const images = [
-//   'images/001.png',
-//   'images/025.png',
-//   'images/039.png',
-//   'images/004.png',
-//   'images/007.png',
+//    'images/001.png',
+//    'images/025.png',
+//    'images/039.png',
+//    'images/004.png',
+//    'images/007.png',
 // ];
 // function slideShow() {
 //   document.querySelector('img').setAttribute('src', images[i]);
@@ -24,7 +24,8 @@
 const images = document.querySelectorAll('img');
 let i = 1;
 
-setInterval(() => {
+// Auto carousel
+setInterval(function carousel() {
   images.forEach((img, i) => {
     img.className = 'hidden';
   });
@@ -34,3 +35,42 @@ setInterval(() => {
   images[i].className = '';
   i++;
 }, 3000);
+
+// Next img
+const nextBtn = document.querySelector('.fa-chevron-right');
+function handleNext(event) {
+  images.forEach((img, i) => {
+    img.className = 'hidden';
+  });
+  if (i === images.length) {
+    i = 0;
+  }
+  images[i].className = '';
+  i++;
+}
+nextBtn.addEventListener('click', handleNext);
+
+// Previous img
+const prevBtn = document.querySelector('.fa-chevron-left');
+function handlePrev(event) {
+  images.forEach((img, i) => {
+    img.className = 'hidden';
+  });
+  if (i === 0) {
+    i = images.length - 1;
+  }
+  images[i].className = '';
+  i--;
+  console.log('prev');
+}
+prevBtn.addEventListener('click', handlePrev);
+
+// Dot progress bar
+const dots = document.querySelectorAll('.dot');
+function handleDots(event) {
+  dots.forEach((dot) => {
+    dot.classList.replace('fa-regular', 'fa-solid');
+  });
+  console.log('clicked');
+}
+dots.addEventListener('click', handleDots);
