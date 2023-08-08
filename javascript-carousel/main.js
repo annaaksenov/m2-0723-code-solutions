09504539443ee02334b796e2f371198f95f8bb5f
Anna Aksenov <annaaksenov8@gmail.com>
2023-08-08 02:50:39 +0000

    Work in progress

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,32 +1,17 @@
-// const images = [
-//    'images/001.png',
-//    'images/025.png',
-//    'images/039.png',
-//    'images/004.png',
-//    'images/007.png',
-// ];
-// function slideShow() {
-//   document.querySelector('img').setAttribute('src', images[i]);
-//   if (i < images.length - 1) {
-//     i++;
-//   } else {
-//     i = 0;
-//   }
-//  setTimeout(slideShow, 3000);
-// }
-// window.onload = slideShow();
-//  const prevBtn = document.querySelector('.fa-chevron-left');
-
-//  prevBtn.addEventListener('click', function() {
-//    /* document.querySelector('img').setAttribute('src', images[i]); */;
-//   console.log('clicked');
-// });
+/* const images = [
+    'images/001.png',
+    'images/025.png',
+    'images/039.png',
+    'images/004.png',
+    'images/007.png',
+ ];
+ */
 const images = document.querySelectorAll('img');
 let i = 1;
 
 // Auto carousel
-setInterval(function carousel() {
-  images.forEach((img, i) => {
+const interID = setInterval(function carousel() {
+  images.forEach((img) => {
     img.className = 'hidden';
   });
   if (i === images.length) {
@@ -39,7 +24,8 @@ setInterval(function carousel() {
 // Next img
 const nextBtn = document.querySelector('.fa-chevron-right');
 function handleNext(event) {
-  images.forEach((img, i) => {
+  clearInterval(interID);
+  images.forEach((img) => {
     img.className = 'hidden';
   });
   if (i === images.length) {
@@ -47,13 +33,15 @@ function handleNext(event) {
   }
   images[i].className = '';
   i++;
+  setInterval(); // tried restarting interval again after incrementing.
 }
 nextBtn.addEventListener('click', handleNext);
 
 // Previous img
 const prevBtn = document.querySelector('.fa-chevron-left');
 function handlePrev(event) {
-  images.forEach((img, i) => {
+  images.forEach((img) => {
+    // Jumps around between imgs but unsure why.
     img.className = 'hidden';
   });
   if (i === 0) {
@@ -69,8 +57,12 @@ prevBtn.addEventListener('click', handlePrev);
 const dots = document.querySelectorAll('.dot');
 function handleDots(event) {
   dots.forEach((dot) => {
-    dot.classList.replace('fa-regular', 'fa-solid');
+    dot.className = 'fa-regular';
   });
+  if (i === dots.length) {
+    i = 0;
+  }
+  dots[i].className = 'fa-solid';
   console.log('clicked');
 }
 dots.addEventListener('click', handleDots);
