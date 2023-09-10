41ecdc925d2a2387428ad1b5a2ff99637ff84431
Anna Aksenov <annaaksenov8@gmail.com>
2023-09-10 21:28:01 +0000

    Completed react-hot-button-component

--- react-hot-button-component/hot-button-app/src/App.jsx
+++ react-hot-button-component/hot-button-app/src/App.jsx
@@ -1,47 +1,41 @@
-/*{import { useState } from 'react'}*/
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useState } from 'react';
 import './App.css';
 
-function HotButton(props) {
-  /*{const [count, setCount] = useState(0);}*/
-  let color = props.className;
-
+function HotButton() {
+  const [count, setCount] = useState(0);
+  const [isColor, setColor] = useState('purple');
+  function handleHotButton() {
+    setCount(count + 1);
+    if (count >= 5) {
+      setColor('lavender');
+    }
+    if (count >= 8) {
+      setColor('pink');
+    }
+    if (count >= 11) {
+      setColor('orange');
+    }
+    if (count >= 14) {
+      setColor('yellow');
+    }
+    if (count >= 17) {
+      setColor('white');
+    }
+  }
   return (
-    <button className={color} onClick={props.handleHotButton}>
-      Hot Button
-    </button>
+    <>
+      <button className={isColor} onClick={handleHotButton}>
+        Hot Button
+      </button>
+      <p>{count}</p>
+    </>
   );
 }
 
 function App() {
-  /*{const [count, setCount] = useState(0)}*/
-  /*{function handleHotButton() {
-    setCount(count + 1)
-    if(count > 3) {
-    }
-  }}*/
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>*/}
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <HotButton className="purple" onClick="handleHotButton" />
+      <HotButton />
     </>
   );
 }
