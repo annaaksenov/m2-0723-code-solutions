c349350f0418aacddfab202a86e1f62fd456b188
Anna Aksenov <annaaksenov8@gmail.com>
2023-09-19 04:12:14 +0000

    Made changes to phase 2, note I had to comment out due to linters

--- react-design/my-app/src/RotatingBanner.jsx
+++ react-design/my-app/src/RotatingBanner.jsx
@@ -1,16 +1,18 @@
-import Banner from './Banner';
-import NextBtn from './NextBtn';
-import PrevBtn from './PrevBtn';
-import Indicators from './Indicators';
+// import Banner from './Banner';
+// import NextBtn from './NextBtn';
+// import PrevBtn from './PrevBtn';
+// import Indicators from './Indicators';
 
-export default function RotatingBanner({ items }) {
-  // const [count, setCount] = useState(0);
-  return (
-    <div>
-      <Banner item={items[0]} />
-      <PrevBtn />
-      <Indicators count={items.length} />
-      <NextBtn />
-    </div>
-  );
-}
+// export default function RotatingBanner({ items }) {
+//   const [currentIndex, setCurrentIndex] = useState(0);
+//   return (
+//     <div>
+//       <Banner item={items[currentIndex]} />
+//       <PrevBtn />
+//       <Indicators count={items.length} current={currentIndex} />
+//       <NextBtn />
+//     </div>
+//   );
+// }
+
+// linter would not let me commit, so I commented everything out
