84ebe7f633edc4e24651234b7bc92ae8b6362da5
Anna Aksenov <annaaksenov8@gmail.com>
2023-09-19 04:24:50 +0000

    Made changes to phase 2

--- react-design/my-app/src/RotatingBanner.jsx
+++ react-design/my-app/src/RotatingBanner.jsx
@@ -1,18 +1,17 @@
-// import Banner from './Banner';
-// import NextBtn from './NextBtn';
-// import PrevBtn from './PrevBtn';
-// import Indicators from './Indicators';
+import { useState } from 'react';
+import Banner from './Banner';
+import NextBtn from './NextBtn';
+import PrevBtn from './PrevBtn';
+import Indicators from './Indicators';
 
-// export default function RotatingBanner({ items }) {
-//   const [currentIndex, setCurrentIndex] = useState(0);
-//   return (
-//     <div>
-//       <Banner item={items[currentIndex]} />
-//       <PrevBtn />
-//       <Indicators count={items.length} current={currentIndex} />
-//       <NextBtn />
-//     </div>
-//   );
-// }
-
-// linter would not let me commit, so I commented everything out
+export default function RotatingBanner({ items }) {
+  const [currentIndex /*setCurrentIndex*/] = useState(0);
+  return (
+    <div>
+      <Banner item={items[currentIndex]} />
+      <PrevBtn />
+      <Indicators count={items.length} current={currentIndex} />
+      <NextBtn />
+    </div>
+  );
+}
